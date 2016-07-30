9c4778894a278ecf33dedf4528bbcecfd34f4b14
lctm2005 <licong2002@163.com>
2016-07-30 11:00:16 +0800

    config-server & config-client

---
+++ spring-cloud-config-server-demo/src/main/java/com/licong/springcloud/configserver/Application.java
@@ -0,0 +1,17 @@
+package com.licong.springcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
+
+//@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
